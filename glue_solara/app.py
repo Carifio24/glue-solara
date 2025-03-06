3000bcb730cca7a5c592266eb9dd5d87618a4285
Carifio24 <carifio24@gmail.com>
2025-03-06 09:41:05 -0500

    Add MDI and layout grid items for existing grid viewers when creating app component.

--- glue_solara/app.py
+++ glue_solara/app.py
@@ -80,9 +80,21 @@ def GlueApp(app: gj.JupyterApplication):
     requested_viewer_for_data_index = solara.use_reactive(None)
     requested_viewer_typename = solara.use_reactive("Scatter")
 
+    def make_mdi_layout(viewer):
+        class_name = viewer.__class__.__name__
+        title = TITLE_TRANSLATIONS.get(class_name, class_name)
+        return {"title": title, "width": 800, "height": 600}
+
+    def make_grid_layout(viewer_index):
+        return {"h": 18, "i": str(viewer_index), "moved": False, "w": 12, "x": 0, "y": 12 * viewer_index}
+
     view_type = solara.use_reactive("tabs")  # tabs, grid, mdi
-    mdi_layouts = solara.use_reactive([])
-    grid_layout = solara.use_reactive([])
+    mdi_layouts = solara.use_reactive([
+        make_mdi_layout(viewer) for viewer in app.viewers
+    ])
+    grid_layout = solara.use_reactive([
+        make_grid_layout(index) for index in range(len(app.viewers))
+    ])
     mdi_header_size_index = solara.use_reactive(2)
 
     def add_data_viewer(type: str, data: glue.core.Data):
@@ -97,20 +109,14 @@ def GlueApp(app: gj.JupyterApplication):
                 error_message.set(str(error))
                 show_error.set(True)
                 return
-        if len(data_collection) == 1:
-            grid_layout.value = [
-                {"h": 18, "i": "0", "moved": False, "w": 12, "x": 0, "y": 0},
-            ]
-        if len(data_collection) == 2:
-            grid_layout.value = [
-                *grid_layout.value,
-                {"h": 18, "i": "0", "moved": False, "w": 12, "x": 18, "y": 0},
-            ]
-        class_name = app.viewers[-1].__class__.__name__
-        title = TITLE_TRANSLATIONS.get(class_name, class_name)
+
+        grid_layout.value = [
+            *grid_layout.value,
+            make_grid_layout(len(app.viewers)-1)
+        ]
         mdi_layouts.value = [
             *mdi_layouts.value,
-            {"title": title, "width": 800, "height": 600},
+            make_mdi_layout(app.viewers[-1])
         ]
         viewer_index.set(len(app.viewers) - 1)
 
